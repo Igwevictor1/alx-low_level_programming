0f19e82f5c107617b4eb00612695aa7ca05fb2c2
Victor <igwevictor11@yahoo.com>
2022-09-08 14:20:52 -0700

    Multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"progrsmming is like building a multilingusl puzzle");
+
+	return (0);
+}
