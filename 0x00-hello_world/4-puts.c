16b4977dae2334c3e17634428ac0c776b088af72
Victor <igwevictor11@yahoo.com>
2022-09-08 14:49:14 -0700

    Multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#includr <stdio.h>
 
 /**
  * main - Entry point
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
