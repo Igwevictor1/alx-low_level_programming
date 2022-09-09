d12f6d2499fbd3365e329de39598624f1440fad8
Victor <igwevictor11@yahoo.com>
2022-09-09 11:37:53 -0700

    Quotting

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - prints out the last part of a quote on the standard error.
+ * main - prints out the last part of a quote in the standard error.
  * Return: 1 if success.
  */
 int main(void)
