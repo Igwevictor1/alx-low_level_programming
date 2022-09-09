eb2b92862209abc48118ce409b6717fbbd9ba47f
Victor <igwevictor11@yahoo.com>
2022-09-08 22:28:55 -0700

    Size of an integer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,5 +18,5 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-	retun (0);
+	return (0);
 }
