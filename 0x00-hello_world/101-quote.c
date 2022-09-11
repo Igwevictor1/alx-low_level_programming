44f3a52c73c60e4f2a8de088f22e159aecd68a49
Victor <igwevictor11@yahoo.com>
2022-09-11 13:27:52 -0700

    Quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
- * main - prints out the last part of a quote in the standard error.
- * Return: 1 if success
+ * main - main function
+ *
+ * Return: end program
  */
+
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 58);
+	char n[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	fprintf(stderr, "%s\n", n);
+
 	return (1);
 }
