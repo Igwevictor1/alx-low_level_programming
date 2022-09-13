a9303677799f11dec9dfaa3fb5a1c91cdf8c6231
Victor <igwevictor11@yahoo.com>
2022-09-13 13:35:19 -0700

    Printing characters

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,23 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	char string[8] = "_putchar";
+	int n = 0;
 
+	while (n < 8)
+	{
+		_putchar(string[n]);
+		++n;
+	}
+
+	_putchar(10);
 	return (0);
+
 }
