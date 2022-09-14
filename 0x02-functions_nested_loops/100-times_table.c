2d547a1d98057e869c95deb352a2cdf4429b312e
Victor <igwevictor11@yahoo.com>
2022-09-14 15:32:06 -0700

    Times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,10 +1,44 @@
-#imclude "main.h"
+#include "main.h"
 /**
- * print_times_table - Entry point
- * @n: input
- * Return: Always 0 (Success)
+ * prints_times_table - printba multiplication table u to param
+ * @n: The number to be treated
+ *
+ * Return: Number matrix
  */
 void print_times_table(int n)
 {
-	return (0);
-}
+	int a, b, op;
+	if (n >0 && n <= 15)
+	{
+		for (a = 0; a <= n; a++)
+		{
+				_putchar(48);
+				for (b = 1; b <= n; b++)
+				{
+					op = a * b;
+					_putchar(44);
+					_putchar(32);
+					if (op <= 9)
+					{
+						_putchar(32);
+						_putchar(32);
+						_putchar(op + 48);
+					}
+					else if (op <=99)
+					{
+						_putchar(32);
+						_putchar((op / 10) + 48);
+						_putchar((op % 10) + 48);
+					}
+					else
+					{
+						_putchar(((op / 100) % 10) + 48);
+						_putchar(((op / 10) % 10) + 48);
+						_putchar((op % 10) + 48);
+					}
+				}
+				_putchar('\n');
+			}
+		}
+	}
+
