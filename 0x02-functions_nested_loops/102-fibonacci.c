e2af6b7dbfb67b438283d89dff12ddf72cbe6c56
Victor <igwevictor11@yahoo.com>
2022-09-13 18:48:13 -0700

    Fibonacci level

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: end program
+ */
+int main(void)
+{
+	long int n, a = 1, b = 1, s;
+
+	printf("%ld", a);
+	for (n = 1; n < 50; ++n)
+	{
+		s = a + b;
+		printf(", %ld", s);
+		a = b;
+		b = s;
+	}
+	_putchar(10);
+
+	return (0);
+}
