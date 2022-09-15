486894a1ca5b44b41b5171864823ca9e418a20d9
Victor <igwevictor11@yahoo.com>
2022-09-15 08:35:05 -0700

    More numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - print the numbers from 0-9 except for 2 and 4.
+ * print_most_numbers - print the numbers from 0-9 except for 2 and 4
  */
 void print_most_numbers(void)
 {
