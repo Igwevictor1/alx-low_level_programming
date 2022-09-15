793f17a6fa5d358d13e410bf5c726ceb4dfecafd
Victor <igwevictor11@yahoo.com>
2022-09-15 02:27:56 -0700

    Header file

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,22 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/*
+ * main - Header file containing prototypes of all functions
+ * used in the 0x03-more_functions_nested_loops directory.
+ *
+ */
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
+#endif
