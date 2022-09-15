6bd4ef1315a8a0d77de2b78a5b579ab7c8ccc0e1
Victor <igwevictor11@yahoo.com>
2022-09-14 17:12:12 -0700

    Still printing fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: end program
+ */
+int main(void)
+{
+	int i = 1;
+	long int num1 = 0;
+	long int num2 = 1;
+	long int sumPrevTwo = 0;
+	long int sumEven = 0;
+	long int maxFibTerm = 4000000;
+
+	while
+		(num1 < maxFibTermm && sum2 < maxFibTerm)
+		{
+			sumPrevTwo = num1 + num2;
+			sumEven += (((sumPrevTwo % 2) == 0) ? sumPrevTwo : 0);
+			num1 = num2;
+			num2 = sumPrevTwo;
+			i++;
+		}
+		printf("%ld\n", sumEvem);
+		return (0);
+}
