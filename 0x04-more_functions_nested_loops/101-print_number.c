39bcb8c7a10848b8d34efde1a5755d5e3c247981
Victor <igwevictor11@yahoo.com>
2022-09-16 06:47:44 -0700

    Print number to base

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * print_number - Print integer
+ * @n: Number
+ */
+void print_number(int n)
+{
+	unsigned int n2, number, base10 = 1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n2 = -n;
+	}
+	else
+	{
+		n2 = n;
+	}
+	number = n2;
+	/* Obtenemos su base */
+	while (number > 9)
+	{
+		number = number / 10;
+		base10 = base10 * 10;
+	}
+	/* Descomponemos en base 10*/
+	number = n2;
+	while (base10 > 1)
+	{
+		_putchar((number / base10) + '0');
+		number = number % base10;
+		base10 = base10 / 10;
+	}
+	_putchar((n2 % 10) + '0');
+}
