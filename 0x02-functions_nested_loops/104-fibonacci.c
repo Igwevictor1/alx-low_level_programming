65f5fdc322f3bac378c5e6e2448afa0ad8e4abdc
Victor <igwevictor11@yahoo.com>
2022-09-17 06:00:09 -0700

    Fibonacci levels

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,52 +1,25 @@
 #include <stdio.h>
-
 /**
- * mian - prints the first 98 fibonacci numbers, starting with
- * 1 and 2, separated by comma followed by a space.
- *
- * Return: Always 0
+ * main - main block
+ * Description: computes and prints the sum of all the multiples of 3or
+ * 5 below 1024 (excluded), followed by a new line
+ * Retuen: 0
  */
 int main(void)
 {
-	int count;
-	unsigned long fib1 = 0, fib2 = 1, sum;
-	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-	unsigned long half1, half2;
+	int i = 0;
+	unsigned long int a = 0, b = 1, next = 0;
 
-	for (count = o; count < 92; count ++)
+	while (i < 98)
 	{
-		sum = fib1 + fib2;
-		printf("%lu, ", sum);
-
-		fib1 = fib2;
-		fib2 = sum;
-	}
-
-	fib1_half1 = fib1 / 10000000000;
-	fib2_half1 = fib2 / 10000000000;
-	fib1_half2 = fib1 % 10000000000;
-	fib2_half2 = fib2 % 10000000000;
-
-	for (count = 93; count < 99; count++)
-	{
-		half1 = fib1_half1 + fib2_half1;
-		half2 = fib1_half2 + fib2_half2;
-
-		if (fib1_half2 + fib2_half2 > 9999999999)
-		{
-			half1 += 1;
-			half2 %= 10000000000;
-		}
-		
-		printf("%lu%lu", half1, half2);
-		if (count != 98)
+		next = a + b;
+		a = b;
+		b = next;
+		printf("%lu", next);
+		if (i < 97)
 			printf(", ");
-
-		fib1_half1 = fib2_half1;
-		fib1_half2 = fib2_half2;
-		fib2_half1 = half1;
-		fib2_half2 = half2;
+		i++;
 	}
-	printf("\n");
+	putchar('\n');
 	return (0);
 }
