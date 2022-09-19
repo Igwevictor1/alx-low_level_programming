e4ec92fc395f65b410e698a156f1edae3a7d06ab
Victor <igwevictor11@yahoo.com>
2022-09-19 00:59:45 -0700

    Resetting file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,12 +1,5 @@
-#ifndef "main.h"
-#define "main.h"
-
-/**
- * main - main.h
- * Description: Header file containing prototypes for all functions
- * used in the 0x05-pointers_arrays_strings directory.
- */
-
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdlib.h>
 
 int _putchar(char c);
@@ -18,8 +11,6 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-void print_array(int *a, int n);
-cahr *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+char *_strcpy(char *dest, char *src);
 
 #endif
