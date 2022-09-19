d3b0040e9607fb27c8f323b324181efe34774373
Victor <igwevictor11@yahoo.com>
2022-09-19 10:53:24 -0700

    Copy string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,15 +2,16 @@
 
 /**
  * _strcpy - Copies a string pointed to by @src.
+ * terminating null byte, to a buffer pointed to by @dest.
  * @dest: A buffer to copy the string to.
- * @src: A source string to copy.
+ * @src: A source string to cpoy.
  *
  * Return: A pointer to the destination string @dest.
  */
 char *_strcpy(char *dest, char *src)
 {
-	int index = 0
-		
+	int index = 0;
+
 	for (index = 0; src[index] != '\0'; index++)
 	{
 		dest[index] = src[index];
