573c410d5da97cac190170babccceb03d51f2735
Victor <igwevictor11@yahoo.com>
2022-09-19 02:59:25 -0700

    Print in revers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,10 +8,10 @@ void print_rev(char *s)
 {
 	int len = 0, i = 0;
 
-	while (S[i++])
+	while (s[i++])
 		len++;
 
-	for (i = len - l; i >= 0; i--)
+	for (i = len - 1; i >= 0; i--)
 		_putchar(s[i]);
 
 	_putchar('\n');
