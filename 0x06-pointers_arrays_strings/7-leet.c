d05b115335e1919db955dbea86f3c02e383f38a4
Victor <igwevictor11@yahoo.com>
2022-09-21 13:14:21 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,28 +2,28 @@
 
 /**
  * leet - Encodes a string to 1337.
- * str: The string to be encoded.
+ * @str: The string to be encoded.
  *
  * Return: A pointer to the encoded string.
  */
 char *leet(char *str)
 {
 	int indx1 = 0, indx2;
-	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+		char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
-	while (str[indx1])
-	{
-			for (indx2 = 0; indx2 <= 7; indx2++)
-			{
-						if (str[indx1] == leet[indx2] ||
-											    str[indx1] - 32 == leet[indx2])
-							str[indx1] = indx2 + '0';
-					}
-	
-			indx1++;
-		}
+			while (str[indx1])
+					{
+								for (indx2 = 0; indx2 <= 7; indx2++)
+											{
+															if (str[indx1] == leet[indx2] ||
+																				    str[indx1] - 32 == leet[indx2])
+																				str[indx1] = indx2 + '0';
+																	}
 
-	return (str);
+										indx1++;
+											}
+
+				return (str);
 }
 Footer
-© 2022 GitHub, Inc.}
+© 2022 GitHub, Inc.
