48099d05b99e3b6d3e200800efaf5a7e64aa894c
Victor <igwevictor11@yahoo.com>
2022-09-27 07:05:40 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,12 +9,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
-
-	return (s);
+	int night = 0; /*Night is my variable and was assigned 0*/
+		while (n > 0) /*as long as n is less than zero*/
+		{
+			s[night] = b; /*Night is assigned to our char*/
+				night++; /*post-increment, value first incremented afterwards*/
+				n--; /*post-decrement*/
+		}
+	return (s); /*returning pointer to the memory of s*/
 }
