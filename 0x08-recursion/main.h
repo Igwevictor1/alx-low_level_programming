7c4e5d72a13f45ac47a598c61a37e8cb534a658b
Victor <igwevictor11@yahoo.com>
2022-10-06 15:39:20 -0700

    My main.h file edited

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,5 +9,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int _putchar(char c);
-int wildcmp(chat *s1, char *s2);
+int wildcmp(char *s1, char *s2);
 #endif
