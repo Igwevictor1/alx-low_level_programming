e7df9eaebe0295b2151a0d7c87f400f3a2fdca0d
Victor <igwevictor11@yahoo.com>
2022-10-12 05:56:15 -0700

    My function_pointers.h file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,10 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
-int _putchar(char c);
+#ifndef _FUNCTION_POINTERS_
+#define _FUNCTION_POINTERS_
+
+#include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int-index(int *array, int size, int (*cmp)(int));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
 #endif
