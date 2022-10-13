d5029918625d0dc3c9e2a3c778ada224b90315a0
Victor <igwevictor11@yahoo.com>
2022-10-13 02:49:02 -0700

    Printing name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * print_name - function to call a function
+ *
+ * @name: parameter 1
+ * @f: parameter function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+	{
+		if (name != NULL)
+		{
+			(*f)(name);
+		}
+	}
+}
