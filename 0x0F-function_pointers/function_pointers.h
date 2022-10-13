92c3a55795527ce70691721ee41e61040095502f
Victor <igwevictor11@yahoo.com>
2022-10-12 21:26:24 -0700

    My header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,16 +1,16 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef HEADER_FILE
+#define HEADER_FILE
 
 int _putchar(char c);
-
 void print_name(char *name, void (*f)(char *));
-
 void array_iterator(int *array, suze_t size, void (*action)(int));
-
 int int_index(int *array, int size, int (*cmp)(int));
-
 int (*get_op_func(char *s))(int, int);
 
-
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 
 #endif
