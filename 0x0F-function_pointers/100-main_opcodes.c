c9f3e44447d3c648831de8a82f6468c0f9ff261d
Victor <igwevictor11@yahoo.com>
2022-10-13 15:19:55 -0700

    hack codes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 
 /**
- * main - check the code for Holberton school students
- * @ergc: argument count.
- * @argv: argument vector.
- * Return: Always 0
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(int argc, char *argv[])
 {
-	char *opc = (char *) main;
-	int i, nbytes;
+	int bytes, i;
+	char *arr;
 
 	if (argc != 2)
 	{
@@ -19,21 +19,23 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
-	nbytes = atoi(argv[1]);
-
-	if (nbytes < 0)
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
 
-	for (i = 0; i < nbytes; i++)
-	{
-		printf("%02x", opc[i] & 0xFF);
-		if (i != nbytes - 1)
-			printf(" ");
-	}
+	arr = (char *)main;
 
-	printf("\n");
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx", arr[i]);
+	}
 	return (0);
 }
