bbc18330ae32053fef08623942f555284fe5a8d7
Victor <igwevictor11@yahoo.com>
2022-10-20 11:28:17 -0700

    My lists.h file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,9 +1,7 @@
- #ifndef _LISTS_ 
- #define _LISTS_ 
+#ifndef _LISTS_H_ 
+ #define _LISTS_H_ 
   
- #include <stdio.h> 
- #include <stdlib.h> 
- #include <string.h> 
+ #include <stddef.h> 
   
  /** 
   * struct list_s - singly linked list 
@@ -21,10 +19,10 @@
          struct list_s *next; 
  } list_t; 
   
+ int _putchar(char); 
  size_t print_list(const list_t *h); 
  size_t list_len(const list_t *h); 
  list_t *add_node(list_t **head, const char *str); 
  list_t *add_node_end(list_t **head, const char *str); 
  void free_list(list_t *head); 
   
- #endif
