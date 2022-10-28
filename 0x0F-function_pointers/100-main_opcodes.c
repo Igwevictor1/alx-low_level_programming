02498cec13fa427dc6e66c0b755700c9b121bb5c
Victor <igwevictor11@yahoo.com>
2022-10-28 06:57:07 -0700

    Opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,32 +10,15 @@
  */
 int main(int argc, char *argv[])
 {
-	int bytes, i;
-	char *arr;
-
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-
-	bytes = atoi(argv[1]);
-	if (bytes < 0)
+	if (atoi(argv[1]) < 0)
 	{
 		printf("Error\n");
 		exit(2);
-	}
-
-	arr = (char *)main;
-
-	for (i = 0; i < bytes; i++)
-	{
-		if (i == bytes - 1)
-		{
-			printf("%02hhx\n", arr[i]);
-			break;
-		}
-		printf("%02hhx", arr[i]);
 	}
 	return (0);
 }
